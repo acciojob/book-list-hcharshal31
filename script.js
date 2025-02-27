8d068e2d065d43145d92001a00888171359e8367
Harshal Chavan <91681985+hcharshal31@users.noreply.github.com>
2025-02-27 16:17:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
 //your JS code here. If required.
+let submitBtn = document.getElementById("submit");
+let table = document.getElementsByClassName("table")[0];
+
+submitBtn.onclick = (ev)=>{
+  ev.preventDefault();
+
+  let title = document.getElementById("title").value;
+  let author = document.getElementById("author").value;
+  let isbn = document.getElementById("isbn").value;
+
+  if(title && author && isbn){
+    table.innerHTML += `
+      <tr>
+        <td>${title}</td>
+        <td>${author}</td>
+        <td>${isbn}</td>
+        <td>
+          <input class="delete" type="button" value="X">
+        </td>
+      </tr>
+    `;
+  } else{
+    alert("Please fill all the form fields")
+  }
+  document.getElementById("title").value = "";
+  document.getElementById("author").value = "";
+  document.getElementById("isbn").value = "";
+}
+
+  document.getElementsByClassName("book-list")[0].onclick = (ev) => {
+    if(ev.target.classList.contains("delete")){
+      ev.target.closest("tr").remove();
+      console.log("Row is removed...");
+    }
+  }
